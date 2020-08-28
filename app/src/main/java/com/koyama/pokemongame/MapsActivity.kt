50e190dd55223ec8be5be950d31c065044b27fa9
hayato <h.jitsu.k@gmail.com>
2020-08-28 17:05:00 +0900

    Permission_setting

--- app/src/main/java/com/koyama/pokemongame/MapsActivity.kt
+++ app/src/main/java/com/koyama/pokemongame/MapsActivity.kt
@@ -1,12 +1,20 @@
 package com.koyama.pokemongame
 
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.os.Build
+import android.os.Build.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -21,8 +29,49 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        checkPermission()
+    }
+    var ACCESSLOCATION=123
+    fun checkPermission(){
+
+        if(Build.VERSION.SDK_INT>=23){
+            if(ActivityCompat
+                    .checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),ACCESSLOCATION)
+                return
+            }
+
+        }
+        GetUserLocation()
     }
 
+    fun GetUserLocation(){
+        Toast.makeText(this, "User Location access on", Toast.LENGTH_LONG).show()
+        //TODO: Will implement later
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+
+            ACCESSLOCATION->{
+
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    GetUserLocation()
+                }
+                else{
+                    Toast.makeText(this, "We can not access to your location", Toast.LENGTH_LONG).show()
+                }
+
+            }
+
+        }
+
+
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -37,7 +86,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addMarker(MarkerOptions()
+            .position(sydney)
+            .title("Curry")
+            .snippet(" here is my location")
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.omu_curry_small)))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14f))
     }
 }
